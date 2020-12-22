677173c904e4a42aa51e430f43589f620d24e132
PabloGaudard <pablogaudard@gmail.com>
2020-12-21 22:38:24 -0300

    c

--- Icon.vue
+++ Icon.vue
@@ -6,6 +6,10 @@
 export default {};
 </script>
 
-<style>
+<style scoped>
 @import url("https://fonts.googleapis.com/css?family=Roboto:300,400,500|Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp");
+
+.material-icons {
+  vertical-align: -27%;
+}
 </style>
