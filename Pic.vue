fd30e9c0dff0d687a20eb84dee70b87e272a7a62
PabloGaudard <pablogaudard@gmail.com>
2021-01-14 11:57:54 -0300

    newpic

---
+++ Pic.vue
@@ -0,0 +1,43 @@
+<template>
+  <div
+    class="pbl-pic"
+    style="background-image: url('image (11).jpg'); margin-bottom: 20px"
+  >
+    <div></div>
+    <div></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.pbl-pic {
+  width: 100%;
+  padding-bottom: 75%;
+  background-size: 0;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-color: #eee;
+  position: relative;
+  overflow: hidden;
+
+  > div {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    background-image: inherit;
+    background-position: inherit;
+    background-repeat: no-repeat;
+    &:nth-of-type(1) {
+      background-size: cover;
+      filter: blur(3px);
+      opacity: 0.6;
+    }
+    &:nth-of-type(2) {
+      background-size: contain;
+    }
+  }
+}
+</style>
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import Icon from "./Icon.vue";
+import Pic from "./Pic.vue";
 import Apax from "./Apax.js";
 
 export default {
     install(Vue) {
         Vue.component("Icon", Icon);
+        Vue.component("Pic", Pic);
     },
     Apax
 }
