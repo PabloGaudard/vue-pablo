887d8f50a0c34541ca1891eb80ac15106aa00b07
PabloGaudard <pablogaudard@gmail.com>
2021-07-02 14:09:01 -0300

    go

---
+++ WhatsApp.vue
@@ -0,0 +1,62 @@
+<template>
+  <a
+    v-bind:href="'https://api.whatsapp.com/send?phone=' + phone"
+    target="_blank"
+    id="wpp"
+  >
+    <div class="ripple"></div>
+    <div class="icon"></div>
+  </a>
+</template>
+
+<script>
+export default {
+  props: ["phone"],
+};
+</script>
+
+<style lang="scss" scoped>
+@keyframes ripple {
+  from {
+    width: 60px;
+    height: 60px;
+    bottom: 10px;
+    right: 10px;
+    opacity: 1;
+  }
+  to {
+    width: 80px;
+    height: 80px;
+    bottom: 0;
+    right: 0;
+    opacity: 0;
+  }
+}
+
+#wpp {
+  position: fixed;
+  z-index: 9;
+  .ripple {
+    background-color: #73af00;
+    position: fixed;
+    bottom: 10px;
+    right: 10px;
+    border-radius: 50%;
+    animation: ripple 1.5s infinite;
+  }
+
+  .icon {
+    width: 60px;
+    height: 60px;
+    background-color: #73af00;
+    position: fixed;
+    bottom: 10px;
+    right: 10px;
+    border-radius: 50%;
+    background-image: url(./assets/wpp.svg);
+    background-size: 50%;
+    background-position: center;
+    background-repeat: no-repeat;
+  }
+}
+</style>
--- index.js
+++ index.js
@@ -1,13 +1,17 @@
+import Apax from "./Apax.js";
+
 import Icon from "./Icon.vue";
 import Pic from "./Pic.vue";
-import Apax from "./Apax.js";
+import WhatsApp from "./WhatsApp.vue";
+
 
 const install = function (Vue) {
     Vue.component("Icon", Icon);
     Vue.component("Pic", Pic);
+    Vue.component("WhatsApp", WhatsApp);
 }
 
-const VuePablo = { Icon, Pic, Apax, install }
+const VuePablo = { Apax, Icon, Pic, WhatsApp, install }
 
 export default VuePablo
-export {Icon, Pic, Apax, install}
+export {Apax, Icon, Pic, WhatsApp, install}
